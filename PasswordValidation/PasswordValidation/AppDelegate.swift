c066eeebc9e4651a8b09bf44c0fce2ccfc541b90
Arunkumar <arunkumar@Mac.lan>
2025-05-25 10:52:49 +0530

    Feat: Remove storyboard reference

---
+++ PasswordValidation/PasswordValidation/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  PasswordValidation
+//
+//  Created by Arunkumar on 25/05/25.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        navigateToHome()
+        
+        return true
+    }
+    
+    private func navigateToHome() {
+        window = UIWindow()
+        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
+    }
+
+
+}
+
---
+++ PasswordValidation/PasswordValidation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PasswordValidation
+//
+//  Created by Arunkumar on 25/05/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemRed
+    }
+
+
+}
+
